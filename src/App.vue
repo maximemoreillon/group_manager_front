6012f4fd43dd65bb15f1ed831f8a66e50ce97ea0
Maxime Moreillon <m.moreillon@gmail.com>
2020-04-09 12:56:07 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <AppTemplate
+      applicationName="Group management microservice"
+      v-bind:navigation="navigation"/>
   </div>
 </template>
 
+<script>
+import AppTemplate from '@moreillon/vue_application_template_flex'
+
+export default {
+  name: 'app',
+  components: {
+    AppTemplate
+  },
+  data(){
+    return{
+      navigation : [
+        {route: '/', label: 'Groups', icon: 'account-multiple'},
+        {route: '/search', label: 'Search', icon: 'account-search'},
+      ]
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueCookies from 'vue-cookies'
+
+Vue.use(VueAxios, axios)
+Vue.use(VueCookies)
 
 Vue.config.productionTip = false
+
+
+router.beforeEach((to, from, next) => {
+  var jwt = Vue.$cookies.get("jwt")
+  if(jwt) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`
+    next();
+  }
+  else {
+    delete axios.defaults.headers.common['Authorization']
+    window.location.href = process.env.VUE_APP_AUTHENTICATION_MANAGER_FRONT_URL;
+  }
+})
 
 new Vue({
   router,
