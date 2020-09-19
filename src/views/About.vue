26af7920d59ca5e00d4bc21afbfdb43f366eb1d4
Maxime Moreillon <m.moreillon@gmail.com>
2020-09-19 05:06:12 +0000

    debugging env

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,7 @@
     <p>Developped by <a href="https://maximemoreillon.com">Maxime MOREILLON</a></p>
     <p>Group manager API URL: {{group_manager_api_url}}</p>
     <p>Authentication API URL: {{authentication_api_url}}</p>
+    <p>user_profile_route: {{!!user_profile_route}}</p>
 
   </div>
 </template>
@@ -21,6 +22,7 @@ export default {
     return {
       group_manager_api_url: process.env.VUE_APP_GROUP_MANAGER_API_URL,
       authentication_api_url: process.env.VUE_APP_AUTHENTICATION_API_URL,
+      user_profile_route: process.env.VUE_APP_USER_PROFILE_ROUTE,
       version: pjson.version
     }
   },
