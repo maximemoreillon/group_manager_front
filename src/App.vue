4686411d8b63c8f16e4cf3c186abb529bc4b48dd
Maxime Moreillon <m.moreillon@gmail.com>
2021-12-06 13:19:38 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <AppTemplate
+    :options="options"
+    @user="get_user($event)">
+
+    <template v-slot:nav>
+      <v-list
+        dense
+        nav >
+        <v-list-item
+          v-for="(item, index) in nav"
+          :key="`nav_item_${index}`"
+          :to="item.to"
+          exact>
+          <v-list-item-icon>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </template>
+
+  </AppTemplate>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AppTemplate from '@moreillon/vue_application_template_vuetify'
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
-}
+  components: {
+    AppTemplate
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data: () => ({
+    options: {
+      title: 'Group manager',
+      authenticate: true,
+      login_url: process.env.VUE_APP_LOGIN_URL,
+      identification_url:process.env.VUE_APP_IDENTIFICATION_URL,
+    },
+    nav: [
+      {title: 'Home', to: {name: 'Home'}, icon: 'mdi-home'},
+      {title: 'About', to: {name: 'About'}, icon: 'mdi-information-outline'},
+    ]
+  }),
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  methods: {
+    get_user(user){
+      console.log(user)
+    }
+  }
+};
+</script>
