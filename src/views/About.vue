60e44d25909f871827eb8d19a13241c1bae9b89e
Maxime Moreillon <m.moreillon@gmail.com>
2023-05-23 14:30:36 +0900

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,72 +1,62 @@
 <template>
   <v-card>
     <v-card-title>Group manager GUI</v-card-title>
+    <v-card-subtitle> v {{ version }} </v-card-subtitle>
     <v-card-text>
-      GUI for the group management microservice. Developed and maintained by <a href="https://maximemoreillon.com">Maxime Moreillon</a>
+      GUI for the group management microservice. Developed and maintained by
+      <a href="https://maximemoreillon.com">Maxime Moreillon</a>
     </v-card-text>
     <v-card-text>
       <v-data-table
         hide-default-footer
         :itemsPerPage="-1"
         :headers="headers"
-        :items="services"/>
+        :items="services"
+      />
     </v-card-text>
-
-
   </v-card>
 </template>
 
 <script>
-import pjson from '@/../package.json'
-export default {
-  name: 'About',
-  data () {
-    return {
-      headers: [
-        {text: 'Service', value: "name"},
-        {text: 'Version', value: "version"},
-        {text: 'URL', value: "url"},
+import pjson from "@/../package.json"
 
+const {
+  VUE_APP_GROUP_MANAGER_API_URL,
+  VUE_APP_IDENTIFICATION_URL,
+  VUE_APP_LOGIN_URL,
+} = process.env
+
+export default {
+  name: "About",
+  data() {
+    return {
+      version: pjson.version,
+      headers: [
+        { text: "Service", value: "name" },
+        { text: "Version", value: "version" },
+        { text: "URL", value: "url" },
       ],
       services: [
         {
-          name: 'Group manager GUI',
+          name: "Group manager GUI",
           url: window.location.origin,
-          version: pjson.version
         },
         {
-          name: 'Group manager back-end',
-          url: process.env.VUE_APP_GROUP_MANAGER_API_URL,
-          version: null
+          name: "Group manager back-end",
+          url: VUE_APP_GROUP_MANAGER_API_URL,
         },
         {
-          name: 'Authentication',
-          url: process.env.VUE_APP_IDENTIFICATION_URL,
-          version: 'N/A',
+          name: "Identification URL",
+          url: VUE_APP_IDENTIFICATION_URL,
+        },
+        {
+          name: "Login URL",
+          url: VUE_APP_LOGIN_URL,
         },
       ],
     }
   },
-  mounted () {
-    this.get_services_version()
-  },
-  methods: {
-
-    get_services_version () {
-      this.services.forEach((service) => {
-        if (service.version) return
-        service.version = 'Connecting...'
-        this.axios.get(service.url)
-          .then(({ data }) => { service.version = data.version })
-          .catch(() => { service.version = 'Unable to connect' })
-      })
-    }
-  }
-
 }
 </script>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
