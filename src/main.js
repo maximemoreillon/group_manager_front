25204040eaa80801e8d7d106a9b63cc75a883769
Maxime Moreillon <m.moreillon@gmail.com>
2024-04-18 18:27:41 +0900

    minor

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from "./store"
 import vuetify from "./plugins/vuetify"
 import axios from "axios"
 import VueAxios from "vue-axios"
-import i18n from "./i18n"
+import i18n from "./plugins/i18n"
 
 axios.defaults.baseURL = process.env.VUE_APP_GROUP_MANAGER_API_URL
 
