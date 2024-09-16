7078bcae2a97b3ed0207d73e87b36d27c2449bac
Maxime Moreillon <m.moreillon@gmail.com>
2024-09-16 13:10:05 +0900

    added oidc support

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,8 @@
 import AppTemplate from "@moreillon/vue_application_template_vuetify"
 import LocaleSelector from "./components/LocaleSelector.vue"
 
+const { VUE_APP_LOGIN_URL, VUE_APP_IDENTIFICATION_URL } = process.env
+
 export default {
   name: "App",
 
@@ -40,8 +42,8 @@ export default {
   data: () => ({
     options: {
       title: "Group manager",
-      login_url: process.env.VUE_APP_LOGIN_URL,
-      identification_url: process.env.VUE_APP_IDENTIFICATION_URL,
+      login_url: VUE_APP_LOGIN_URL,
+      identification_url: VUE_APP_IDENTIFICATION_URL,
     },
   }),
 
--- src/main.js
+++ src/main.js
@@ -6,17 +6,43 @@ import vuetify from "./plugins/vuetify"
 import axios from "axios"
 import VueAxios from "vue-axios"
 import i18n from "./plugins/i18n"
+import OidcAuth from "@moreillon/oidc-auth"
 
-axios.defaults.baseURL = process.env.VUE_APP_GROUP_MANAGER_API_URL
+const {
+  VUE_APP_GROUP_MANAGER_API_URL,
+  VUE_APP_OIDC_AUTHORITY,
+  VUE_APP_OIDC_CLIENT_ID,
+} = process.env
+
+axios.defaults.baseURL = VUE_APP_GROUP_MANAGER_API_URL
 
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
-new Vue({
+const vueApp = new Vue({
   router,
   store,
   vuetify,
   i18n,
   render: (h) => h(App),
-}).$mount("#app")
+})
+
+if (VUE_APP_OIDC_AUTHORITY && VUE_APP_OIDC_CLIENT_ID) {
+  const auth = new OidcAuth({
+    authority: VUE_APP_OIDC_AUTHORITY,
+    client_id: VUE_APP_OIDC_CLIENT_ID,
+  })
+  auth.init().then((user) => {
+    if (!user) return
+
+    vueApp.$mount("#app")
+    axios.defaults.headers.common["Authorization"] = `Bearer ${user.id_token}`
+  })
+
+  auth.userManager.events.addUserLoaded((user) => {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${user.id_token}`
+  })
+} else {
+  vueApp.$mount("#app")
+}
