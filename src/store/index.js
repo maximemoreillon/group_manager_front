060ebae32c7633323c2afd842bbbd6bfe9b071f1
Leah Joy Ishiguro <leah_ishiguro@jtekt.co.jp>
2026-01-29 09:06:55 +0900

    Fix logic

--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,7 @@ export default new Vuex.Store({
   },
   mutations: {
     set_current_user(state, user) {
-      if (!user) {
-        state.current_user = null;
-        return;
-      }
-
-      state.current_user = user.profile || user;
+      state.current_user = user?.profile || user;
     },
     set_tokens(state, tokens) {
       state.tokens = tokens;
